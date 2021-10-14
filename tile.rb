b2c4083b2aa237a8b76bec6be6545d0bbb0b5730
Yato Aki <yatoakii@gmail.com>
2021-10-14 13:52:25 +0630

    Added basic requirements to Tile Class

---
+++ tile.rb
@@ -0,0 +1,27 @@
+require 'colorize'
+
+class Tile
+    attr_reader :flagged,:revealed
+    def initialize(bombed = false)
+        @bombed = bombed
+        @flagged  = false
+        @revealed = false
+    end
+
+    def reveal
+        @revealed = true
+    end
+
+    def to_s
+        return "F".colorize(:red) if @flagged
+        @revealed ? "_" : "*"
+    end
+
+    def flag
+        @flagged = true
+    end
+
+    def explode?
+        @bombed
+    end
+end
